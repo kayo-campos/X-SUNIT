8acfbb22c68e5d2b4c76eb582832c2d6f814df65
PuckmanXY <kayoanderson0403@gmail.com>
2018-08-05 00:09:08 -0300

    Some code refactoring in abduction_reports_controller create function

--- app/controllers/api/v1/abduction_reports_controller.rb
+++ app/controllers/api/v1/abduction_reports_controller.rb
@@ -9,30 +9,37 @@ module Api
                         status: 'ERROR',
                         message: "A survivor can't report his/her/hxr own abduction"
                     }
+                ## We all know you get nervous when a friend of yours gets abducted by an alien, but if we still care about design patterns in an alien apocalypse, why shouldn't we care about saving some database storage (and other problems, of course)?
+                elsif AbductionReport.find_by(witness_id: params[:witness_id], survivor_id: params[:survivor_id]) != nil
+                    render json: {
+                        status: 'ERROR',
+                        message: "Witness already reported abduction"
+                    }
                 else
+                ## Having a reference to survivor and witness objects, we make sure they exist
                     survivor = Survivor.find(params[:survivor_id])
                     witness = Survivor.find(params[:witness_id])
-                    abductionReport = AbductionReport.new(abductionReport_params)
+                    abductionReport = AbductionReport.new(abduction_report_params)
                     abductionReport.save
-                    witnessesReportingAbduction = findWitnesses(params[:survivor_id], params[:witness_id])
+                    witnessesReportingAbduction = find_witnesses(params[:survivor_id], params[:witness_id])
                     if witnessesReportingAbduction >= 3 and survivor[:abducted] == false
                         survivor.update(abducted: true)
                     end
                     render json: {
                         status: 'SUCCESS',
-                        survivor: survivor,
-                        abductionReport: abductionReport,
-                        witnessesReportingAbduction: witnessesReportingAbduction
-                    }
+                        data: abductionReport
+                    }, status: :ok
+
                 end
+
             end
 
             private
-            def abductionReport_params
+            def abduction_report_params
                 params.permit(:survivor_id, :witness_id)
             end
 
-            def findWitnesses(survivor_id, witness_id)
+            def find_witnesses(survivor_id, witness_id)
                 survivorAbductionReports = AbductionReport.where('survivor_id == ?', survivor_id)
                 witnessesReportingAbduction = survivorAbductionReports.map { |report| report[:witness_id] }
                 witnessesReportingAbduction = witnessesReportingAbduction.uniq.count
