c844e0a1c5ed3ea86b4de56bc22c86137b326410
PuckmanXY <kayoanderson0403@gmail.com>
2018-08-05 21:19:19 -0300

    Some more validation for reporting an abduction

--- app/controllers/api/v1/abduction_reports_controller.rb
+++ app/controllers/api/v1/abduction_reports_controller.rb
@@ -2,33 +2,45 @@ module Api
     module V1
         class AbductionReportsController < ApplicationController
             def create
+                ## Having a reference to survivor and witness objects, we make sure they exist
+                survivor = Survivor.find(params[:survivor_id])
+                witness = Survivor.find(params[:witness_id])
                 ## A survivor shouldn't be able to report his own abduction since he's probably in the UFO
-                if params[:survivor_id] == params[:witness_id]
-                    render json: {
-                        status: 'ERROR',
-                        message: "A survivor can't report his/her/hxr own abduction"
+                if survivor.id == witness.id
+                    status = 'ERROR'
+                    data = {
+                        message: "a survivor can't report his/her own abduction"
                     }
-                ## We all know you get nervous when a friend of yours gets abducted by an alien, but if we still care about design patterns in an alien apocalypse, why shouldn't we care about saving some database storage (and other problems, of course)?
+                    statusCode = 400
+                ## If a survivor already reported antoher survivor abduction, then the array below should be empty
                 elsif AbductionReport.find_by(witness_id: params[:witness_id], survivor_id: params[:survivor_id]) != nil
-                    render json: {
-                        status: 'ERROR',
-                        message: "Witness already reported abduction"
+                    status = 'ERROR'
+                    data = {
+                        message: "witness already reported this abduction"
+                    }
+                    statusCode = 400
+                ## If an abducted survivor is reporting another survivor abduction, then we have a problem
+                elsif witness.abducted == true
+                    status = 'ERROR'
+                    data = {
+                        message: "an abducted survivor can't report another survivor abduction"
                     }
                 else
-                ## Having a reference to survivor and witness objects, we make sure they exist
-                    survivor = Survivor.find(params[:survivor_id])
-                    witness = Survivor.find(params[:witness_id])
                     abductionReport = AbductionReport.new(abduction_report_params)
                     abductionReport.save
                     witnessesReportingAbduction = find_witnesses(params[:survivor_id], params[:witness_id])
                     if witnessesReportingAbduction >= 3 and survivor[:abducted] == false
                         survivor.update(abducted: true)
                     end
-                    render json: {
-                        status: 'SUCCESS',
-                        data: abductionReport
-                    }, status: :ok
+                    status = 'SUCCES'
+                    data = {
+                        message: "abduction reported",
+                        abduction_report: abductionReport
+                    }
+                    statusCode = 200
                 end
+
+                render_response(status, data, statusCode)
             end
 
             private
@@ -42,6 +54,15 @@ module Api
                 witnessesReportingAbduction = survivorAbductionReports.map { |report| report[:witness_id] }.uniq.count
                 return witnessesReportingAbduction
             end
+
+            ## Side-function to handle HTTP responses, since they follow a pattern of STATUS_CODE, STATUS_MESSAGE and DATA
+            def render_response(status, data, statusCode)
+                render json: {
+                    status: status,
+                    data: data
+                }, status: statusCode
+            end
+
         end
     end
 end
